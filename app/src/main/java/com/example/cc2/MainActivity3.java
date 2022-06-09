cd0f8f921fa0f75f332a8d2c5d564f5125a5c823
Tabaze <83618505+Tabaze@users.noreply.github.com>
2022-06-09 16:05:14 +0100

    Question 8 d

--- app/src/main/java/com/example/cc2/MainActivity3.java
+++ app/src/main/java/com/example/cc2/MainActivity3.java
@@ -17,7 +17,6 @@ public class MainActivity3 extends AppCompatActivity {
     Button eff,en;
     String[] pays;
     EditText ville,tem,pou;
-    String pay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
