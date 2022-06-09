c6c1cb72585790b33ecd0166370a524145b428d7
Tabaze <83618505+Tabaze@users.noreply.github.com>
2022-06-09 14:21:38 +0100

    Question 4

--- app/src/main/java/com/example/cc2/MyDataClimat.java
+++ app/src/main/java/com/example/cc2/MyDataClimat.java
@@ -1,14 +1,30 @@
 package com.example.cc2;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class MyDataClimat extends SQLiteOpenHelper {
-    String name="Climat.db";
-    String Table="";
+    public static String dbName="Climat.db";
+    public static String Table="Climat";
+    public static String col1="id";
+    public static String col2="ville";
+    public static String col3="pays";
+    public static String col4="temperature";
+    public static String col5="PourcNuage";
+
+    public MyDataClimat(Context context) {
+        super(context, dbName, null, 1);
+    }
+
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String sql = "CREATE TABLE " + Table + " (" + col1 + " integer primary key autoincrement," + col2 + " text," + col3 + " text," + col4 + " integer," + col5 + " integer)";
+        sqLiteDatabase.execSQL(sql);
     }
 
     @Override
