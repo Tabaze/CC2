703a25b7afeeff0e930da58e52566d83a560c7f3
Tabaze <83618505+Tabaze@users.noreply.github.com>
2022-06-09 14:39:54 +0100

    Question 6

--- app/src/main/java/com/example/cc2/MainActivity.java
+++ app/src/main/java/com/example/cc2/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    MyDataClimat db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db =new MyDataClimat(this);
+        ArrayList<Climat> climates =new ArrayList<>();
+        climates.add(new Climat(1,22,12,"Martil","Maroc"));
+        climates.add(new Climat(2,22,12,"Tetouan","Maroc"));
+        climates.add(new Climat(3,22,12,"Casa","Maroc"));
+        climates.add(new Climat(4,22,12,"Paris","France"));
+        climates.add(new Climat(5,22,12,"London","Uk"));
+        for(Climat cl:climates)
+         MyDataClimat.AddClimat(db.getWritableDatabase(),cl);
     }
 }
