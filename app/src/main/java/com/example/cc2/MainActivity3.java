4c1c9889c3a76c17267816ab3b2415ad0177a60b
Tabaze <83618505+Tabaze@users.noreply.github.com>
2022-06-09 15:32:09 +0100

    Question 8 b

--- app/src/main/java/com/example/cc2/MainActivity3.java
+++ app/src/main/java/com/example/cc2/MainActivity3.java
@@ -3,12 +3,18 @@ package com.example.cc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        sp=findViewById(R.id.spPays);
+        String[] pays = getResources().getStringArray(R.array.pays);
+        ArrayAdapter ad = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,pays);
+        sp.setAdapter(ad);
     }
 }
